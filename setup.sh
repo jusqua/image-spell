f49efac4610395e6a9858d7f3478422f4dab1def
Ádrian Gama <adrian.gama.se@gmail.com>
2022-06-13 12:03:23 -0300

    fix: bad ui compilation

--- setup.sh
+++ setup.sh
@@ -1,36 +1,19 @@
-#! /bin/sh
+#!/bin/bash
 
-exec pyside6-uic data/template/mainwindow.ui -o mainwindow.py & pid=$!
-wait $pid
+echo "Compiling UI..."
+pyside6-uic data/template/mainwindow.ui -o mainwindow.py
+pyside6-uic data/template/resize.ui -o resize.py
+pyside6-uic data/template/info.ui -o info.py
+pyside6-uic data/template/settings.ui -o settings.py
 
-exec pyside6-uic data/template/resize.ui -o resize.py & pid=$!
-wait $pid
+echo "Merging File..."
+cat mainwindow.py > data/template/design.py
+cat resize.py >> data/template/design.py
+cat info.py >> data/template/design.py
+cat settings.py >> data/template/design.py
 
-exec pyside6-uic data/template/info.ui -o info.py & pid=$!
-wait $pid
+echo "Removing Garbage..."
+rm -rf mainwindow.py resize.py info.py settings.py
 
-exec pyside6-uic data/template/settings.ui -o settings.py & pid=$!
-wait $pid
+echo "Process complete!"
 
-exec tail -n +11 mainwindow.py > data/template/design.py & pid=$!
-wait $pid
-
-exec tail -n +15 resize.py >> data/template/design.py & pid=$!
-wait $pid
-
-exec tail -n +15 info.py >> data/template/design.py & pid=$!
-wait $pid
-
-exec tail -n +15 settings.py >> data/template/design.py & pid=$!
-wait $pid
-
-exec rm -rf mainwindow.py resize.py info.py settings.py & pid=$!
-wait $pid
-
-exec sed -i '/^\s*#/ d' data/template/design.py & pid=$!
-wait $pid
-
-exec sed -i '1i\"""Auto generated Qt User Interface"""\n' data/template/design.py & pid=$!
-wait $pid
-
-echo "-- Process complete --"
